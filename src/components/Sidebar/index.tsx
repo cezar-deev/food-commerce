54524b9e2ee236d6d2fe29cbc5f08b207de6316b
Cezar Cardoso <cezar.deev@gmail.com>
2024-01-22 15:22:36 -0300

    Ate item 2.3

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,23 @@
 import { Container } from "./styles"
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+import menuImg from '../../assets/menu.svg'
 
 export function Sidebar () {
-  return <Container>Sidebar</Container>
+    return (
+    <Container>
+      <button type="button">
+        <img src={menuImg} alt="Abrir e fechar o menu"/>
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">
+              <BurgerIcon />
+              <span>Hamburgueres</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  )
 }
