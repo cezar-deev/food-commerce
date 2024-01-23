a93f5dd5458eecffbca7ed384a45221020bc36d5
Cezar Cardoso <cezar.deev@gmail.com>
2024-01-23 19:34:12 -0300

    ok

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,9 @@
 import { Container } from "./styles"
 import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
+import { ReactComponent as SodaIcon } from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
+
 import menuImg from '../../assets/menu.svg'
 
 export function Sidebar () {
@@ -14,6 +18,24 @@ export function Sidebar () {
             <a href="#" className="active">
               <BurgerIcon />
               <span>Hamburgueres</span>
+            </a>
+          </li>
+           <li>
+            <a href="#" >
+              <PizzaIcon />
+              <span>Pizzas</span>
+            </a>
+          </li>
+           <li>
+            <a href="#" >
+              <SodaIcon />
+              <span>Bebidas</span>
+            </a>
+          </li>
+           <li>
+            <a href="#" >
+              <IceCreamIcon />
+              <span>Sorvetes</span>
             </a>
           </li>
         </ul>
