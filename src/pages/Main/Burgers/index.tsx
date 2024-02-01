d5b7839cf91ca653d773dd2e976fca5fb84cf835
Cezar Cardoso <cezar.deev@gmail.com>
2024-02-01 13:27:21 -0300

    ok

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect} from "react"
 
-
 import { Head } from "../../../components/Head"
 import { SnackTitle } from "../../../components/SnackTitle"
 import { Snacks } from "../../../components/Snacks"
