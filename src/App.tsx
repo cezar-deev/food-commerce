6f9ba9b5b6aa034c3fc65c3c26ea7b6c3f2f8c8f
Cezar Cardoso <cezar.deev@gmail.com>
2024-02-09 13:12:11 -0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,49 @@ import { Theme } from './styles/Theme'
 import { GlobalStyle } from './styles/global'
 import { Normalize } from 'styled-normalize'
 import { SnackData } from './interfaces/SnackData'
-import { getBurgers } from './services/api'
+import { getBurgers, getDrinks, getPizzas, getIceCreams } from './services/api'
 
 interface SnackContextProps {
   burgers: SnackData[]
-  //pizza: SnackData[]
-  //drinks: SnackData[]
-  //iceCreams: SnackData[]
+  pizza: SnackData[]
+  drinks: SnackData[]
+  iceCreams: SnackData[]
 
 }
 export const SnackContext = createContext( {} as SnackContextProps)
 
 export default function App() {
   const [burgers, setBurgers] = useState<SnackData[]>([])
+  const [pizza, setPizzas]= useState<SnackData[]>([])
+  const [drinks, setDrinks] = useState<SnackData[]>([])
+  const [iceCreams, setIceCreams] = useState<SnackData[]>([])
+
   useEffect(() => {
     (async () => {
-      const BurgersRequest = await getBurgers()
-      setBurgers(BurgersRequest.data)
-    })()
-  },[])
+      try {
+        const BurgersRequest = await getBurgers()
+        const pizzaRequest = await getPizzas()
+        const drinkRequest = await getDrinks()
+        const iceCreamRequest = await getIceCreams()
+
+        const  requests = [BurgersRequest, pizzaRequest, drinkRequest, iceCreamRequest]
+
+        const [
+          { data: burguerResponse },
+          { data: pizzaResponse },
+          { data: drinkResponse },
+          { data: iceCreamResponse },
+        ] = await Promise.all(requests)
+
+        setBurgers(burguerResponse)
+        setPizzas(pizzaResponse)
+        setDrinks(drinkResponse)
+        setIceCreams(iceCreamResponse)
+      } catch (error) {
+        console.error(error)
+      }
+    } )()
+  }, [])
 
   return (
     <BrowserRouter>
