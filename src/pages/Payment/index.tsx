dc70702bcd894fac07bf5a8e8c6a54bc6a1fce02
Cezar Cardoso <cezar.deev@gmail.com>
2024-03-23 13:12:56 -0300

    ok

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -137,6 +137,50 @@ export  default function Payment() {
                 <option value='DF'>Distrito Federal</option>
               </select>
             </div>
+          </div>
+
+          <h4>Pagamento</h4>
+
+          <div className='field'>
+            <label htmlFor='credit-card-number'>Número do cartão</label>
+            <input
+              type='text'
+              id='credit-card-number'
+              name='credit-card-number'
+              autoComplete='cc-number'
+            />
+          </div>
+
+          <div className='field'>
+            <label htmlFor='credit-card-holder-name'>Nome impresso no cartão</label>
+            <input
+              type='text'
+              id='credit-card-holder-name'
+              name='credit-card-holder-name'
+              autoComplete='cc-name'
+            />
+          </div>
+
+          <div className='grouped'>
+            <div className='field'>
+              <label htmlFor='credit-card-expiration'>Validade (MM/AA)</label>
+              <input
+                type='text'
+                id='credit-card-expiration'
+                name='credit-card-expiration'
+                autoComplete='cc-exp'
+              />
+            </div>
+
+            <div className='field'>
+              <label htmlFor='credit-card-code'>Código de segurança (CVV)</label>
+              <input
+                type='text'
+                id='credit-card-code'
+                name='credit-card-code'
+                autoComplete='cc-csc'
+              />
+            </div>
           </div> 
         </Form>
       </Inner>
