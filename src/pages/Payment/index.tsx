4401bdb64e55636bb96c4b940b7e34c83ceb6b87
Cezar Cardoso <cezar.deev@gmail.com>
2024-03-23 13:50:59 -0300

    ok

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,4 +1,5 @@
 import { Head } from "../../components/Head";
+import { PayOrder } from "../../components/OrderCloseAction/PayOrder";
 import { OrderHeader } from "../../components/OrderHeader";
 import { Container, Inner, Form } from "./styles";
 
@@ -183,6 +184,7 @@ export  default function Payment() {
             </div>
           </div> 
         </Form>
+        <PayOrder />
       </Inner>
     </Container>
   )
